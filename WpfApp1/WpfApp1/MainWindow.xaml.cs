4f3524cd8c7d9f3796bdcae9e480e78b30a301b9
Jaysen Stas <jaysen.stas@student.pxl.be>
2024-11-17 05:25:59 +0100

    Mastermind-01

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MastermindGame
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string[] _colors = { "Red", "Yellow", "Orange", "White", "Green", "Blue" };
+        private string[] _generatedCode; // Private variable to store the generated code
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        
